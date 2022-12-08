c5022db7c318b289c0351dce5ca9ab8f8096b22f
palucki <piotrek.palucki@gmail.com>
2022-12-08 22:26:26 +0100

    Fixed time formatting

--- drinq-ui-simple/js/common.js
+++ drinq-ui-simple/js/common.js
@@ -1,33 +1,42 @@
 function updateTimeSinceLastDrink(diff_secs) {
+    const preamble = qsTr("last consumed drink: ")
+    const suffix = qsTr(" ago")
+
     if(diff_secs === -1)
     {
         return ""
     }
-    else if(diff_secs <= 60)
+    else if(diff_secs < 60)
     {
-        return qsTr("last consumed drink: less than a minute ago")
+        return preamble + qsTr("less than a minute") + suffix
+
     }
-    else if(diff_secs <= 3600)
+    else if(diff_secs < 3600)
     {
         const diff_mins = Math.floor(diff_secs / 60)
-        return qsTr("last consumed drink: ") + diff_mins + qsTr(" minutes ago")
+        const mins_string = (diff_mins > 1 ? diff_mins + qsTr(" minutes") : qsTr("1 minute"))
+        return preamble + mins_string + suffix
     }
-    else if(diff_secs <= 86400)
+    else if(diff_secs < 86400)
     {
         const diff_hours = Math.floor(diff_secs / 3600)
-        const diff_mins = Math.floor((86400 - diff_secs) / 60)
-        return qsTr("last consumed drink: ")
-                                         + diff_hours + qsTr(" hour ")
-                                         + diff_mins + qsTr(" minutes ago ")
+        const diff_mins = Math.floor((diff_secs - (diff_hours * 3600)) / 60)
+
+        const hours_string = (diff_hours > 1 ? diff_hours + qsTr(" hours ") : qsTr("1 hour"))
+        const mins_string = (diff_mins == 0 ? "" : diff_mins == 1 ? qsTr(" 1 minute") : diff_mins + qsTr(" minutes"))
+
+        return preamble + hours_string + mins_string + suffix
     }
     else
     {
         const diff_days = Math.floor(diff_secs / 86400)
         const diff_hours = Math.floor((diff_secs - (diff_days * 86400)) / 3600)
         const diff_mins = Math.floor((diff_secs - (diff_days * 86400) - (diff_hours * 3600)) / 60)
-        return qsTr("last consumed drink: ")
-                                         + diff_days + qsTr(" days ")
-                                         + diff_hours + qsTr(" hours ")
-                                         + diff_mins + qsTr(" minutes ago ")
+
+        const days_string =  (diff_days > 1 ? diff_days + qsTr(" days") : qsTr("1 day"))
+        const hours_string = (diff_hours > 1 ? diff_hours + qsTr(" hours ") : qsTr("1 hour"))
+        const mins_string = (diff_mins == 0 ? "" : diff_mins == 1 ? qsTr(" 1 minute") : diff_mins + qsTr(" minutes"))
+
+        return preamble + days_string + hours_string + mins_string + suffix
     }
 }
